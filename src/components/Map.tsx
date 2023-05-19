cb17dcd1674c3cdee588d982e0e213cffabc57b7
Fernando Martínez <f.martinez09@ufromail.cl>
2023-05-19 17:27:08 -0400

    envolver componente de map y layour con next-theme

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,22 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
+import { useTheme } from 'next-themes'
 
 const DynamicMap = dynamic(() => import("./DynamicMap"), { ssr: false });
 
 const Map: React.FC = () => {
-  return <DynamicMap />;
+    const { theme, setTheme } = useTheme()
+    return (
+        <div>
+            <DynamicMap />
+            <div>
+                <h1>El tema actual es: {theme}</h1>
+                <button onClick={() => setTheme('light')}>Modo claro</button>
+                <button onClick={() => setTheme('dark')}>Modo oscuro</button>
+            </div>
+        </div>
+    );
 };
 
 export default Map;
