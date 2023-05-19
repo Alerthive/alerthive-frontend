87daff19eff71726aaf71cbf40c559005b40ac0c
Diego Pincheira <d.pincheira01@ufromail.cl>
2023-05-19 05:43:29 -0400

    feat(map view): add map component and implement css, navbar implements with button "configuration" (partially) BREAKING CHANGE: implements of mapbox library to use map in frontend.

---
+++ src/components/Map.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+
+const DynamicMap = dynamic(() => import("./DynamicMap"), { ssr: false });
+
+const Map: React.FC = () => {
+  return <DynamicMap />;
+};
+
+export default Map;
